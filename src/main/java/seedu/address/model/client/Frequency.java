3ecdc257dd0e3bdf5839616d9937123fdccdd705
LAPTOP-OR4J58MA\TANG YUKUN <e1297789@u.nus.edu>
2025-04-03 18:13:44 +0800

    give clear statement when the input of frequency exceed int's limit

--- src/main/java/seedu/address/model/client/Frequency.java
+++ src/main/java/seedu/address/model/client/Frequency.java
@@ -7,7 +7,7 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
  * Represents a Client's frequency of product purchasing.
  */
 public class Frequency {
-    public static final String MESSAGE_CONSTRAINTS = "Frequency should be a non-negative integer";
+    public static final String MESSAGE_CONSTRAINTS = "Frequency should be a non-negative integer and should not exceed 2000000000";
 
     public final int frequency;
 
