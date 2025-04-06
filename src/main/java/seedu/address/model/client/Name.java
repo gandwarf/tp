8bca4efb8bbd6d320a1e038a9df346146ae65562
LAPTOP-OR4J58MA\TANG YUKUN <e1297789@u.nus.edu>
2025-04-06 22:21:35 +0800

    Remove the acceptance of number in name

--- src/main/java/seedu/address/model/client/Name.java
+++ src/main/java/seedu/address/model/client/Name.java
@@ -17,7 +17,7 @@ public class Name {
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "^(?=.{1,40}$)[a-zA-Z0-9]+( [a-zA-Z0-9]+)*$";
+    public static final String VALIDATION_REGEX = "^(?=.{1,40}$)[a-zA-Z]+( [a-zA-Z]+)*$";
 
     public final String fullName;
 
